6f12f4eab3190713a6ecbdf0e460c6e58e512826
andrearomeo04 <pisco004@gmail.com>
2024-06-26 17:23:15 +0200

    [UPD] added button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 let num_km = document.getElementById('km').value;
 let km_price = num_km * 0.21;
-console.log(num_km)
+
 
 let age = document.getElementById('age').value;
-console.log(age)
 
 let final_price = km_price
 
-if (age < 18) {
-    let discount = km_price * 0.2
-    final_price = km_price - discount
-}
 
-else if (age > 65) {
-    let discount = km_price * 0.4
-    final_price = km_price - discount
-}
+const btn = document.getElementById('calculate_price')
+btn.addEventListener('click', function () {
+    num_km
+    age
 
-let price = final_price.toFixed(2)
-console.log(price)
+    if (age < 18) {
+        let discount = km_price * 0.2
+        final_price = km_price - discount
+    }
+    
+    else if (age > 65) {
+        let discount = km_price * 0.4
+        final_price = km_price - discount
+    }
 
+    let price = final_price.toFixed(2)
+
+    console.log(num_km, age, price);
+})
